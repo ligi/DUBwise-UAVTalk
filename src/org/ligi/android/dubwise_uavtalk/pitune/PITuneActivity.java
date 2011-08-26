32a67c04289573dd28cc2ce3314d895c18f7a694
ligi <ligi@ligi.de>
2011-08-26 13:10:38 +1200

    add options-menu with entry to get help from http://www.openpilot.org/pid-tuning

--- src/org/ligi/android/dubwise_uavtalk/pitune/PITuneActivity.java
+++ src/org/ligi/android/dubwise_uavtalk/pitune/PITuneActivity.java
@@ -1,12 +1,18 @@
 package org.ligi.android.dubwise_uavtalk.pitune;
 
+import org.ligi.android.common.intents.IntentHelper;
 import org.ligi.android.uavtalk.dubwise.R;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PITuneActivity extends FragmentActivity {
 			
@@ -36,6 +42,28 @@ public class PITuneActivity extends FragmentActivity {
 				return PITuneFragment.newInstance(arg0) ;
 			}
 	   }
+
+	    private final static int MENU_SAVE_VIDEO=0;
+	    
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        menu.clear();
+	        menu.add(0,MENU_SAVE_VIDEO,0,"Help (Online-Video)").setIcon(android.R.drawable.ic_menu_help);
+	        return super.onCreateOptionsMenu(menu);
+	    }
+
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch(item.getItemId()) {
+	        case MENU_SAVE_VIDEO:
+	        	Intent i= new Intent( "android.intent.action.VIEW",
+	        			Uri.parse( "http://www.openpilot.org/pid-tuning"));
+	        
+	        	this.startActivity(i);
+	        	break;
+	        }
+	        return true;
+	    }
 
 	 
 }
