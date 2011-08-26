f32ef2b42a47e285092403627a73d02315bbad7d
ligi <ligi@ligi.de>
2011-08-26 16:17:18 +1200

    add android helper functions around persisting UAVObjects

---
+++ src/org/ligi/android/dubwise_uavtalk/uavobject_browser/UAVObjectPersistHelper.java
@@ -0,0 +1,71 @@
+package org.ligi.android.dubwise_uavtalk.uavobject_browser;
+
+import org.ligi.android.dubwise_uavtalk.connection.UAVTalkGCSThread;
+import org.openpilot.uavtalk.UAVObject;
+import org.openpilot.uavtalk.UAVObjects;
+import org.openpilot.uavtalk.UAVTalkDefinitions;
+import org.openpilot.uavtalk.uavobjects.ObjectPersistence;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+/**
+ * android helper functions around persisting UAVObjects
+ * 
+ * @author ligi
+ *
+ */
+public class UAVObjectPersistHelper {
+
+	public static void persistWithDialog(Context ctx,final UAVObject obj) {
+		
+		final int DIALOG_DISMISS = 0;
+
+
+		final AlertDialog dialog = ProgressDialog.show(ctx, "Saving",
+				"writing the values to your  board", true, true);
+		final Handler handler = new Handler() {
+			public void handleMessage(Message msg) {
+			
+					dialog.dismiss();
+
+			}
+		};
+
+		new Thread() {
+			public void run() {
+				apply(obj);
+				save(obj);
+				int i = 0;
+				while (UAVObjects.getObjectPersistence().getOperation() != ObjectPersistence.OPERATION_COMPLETED)
+					try {
+						Thread.sleep(100);
+						i++;
+
+						if ((i % 23) == 0) { // every 2.3s reinitiate apply/save
+							apply(obj);
+							save(obj);
+						}
+
+					} catch (InterruptedException e) {
+					}
+
+				handler.sendEmptyMessage(DIALOG_DISMISS);
+			}
+		}.start();
+	}
+	
+	public final static void apply(UAVObject obj) {
+    	UAVTalkGCSThread.getInstance().send_obj(obj,UAVTalkDefinitions.TYPE_OBJ_ACK);
+    }
+	
+	public final static void save(UAVObject obj) {
+    	UAVObjects.getObjectPersistence().setOperation(ObjectPersistence.OPERATION_SAVE);
+    	UAVObjects.getObjectPersistence().setObjectID(obj.getObjID());
+    	UAVObjects.getObjectPersistence().setSelection(ObjectPersistence.SELECTION_SINGLEOBJECT);
+    	UAVTalkGCSThread.getInstance().send_obj(UAVObjects.getObjectPersistence(), UAVTalkDefinitions.TYPE_OBJ_ACK);
+    }
+ 
+}
