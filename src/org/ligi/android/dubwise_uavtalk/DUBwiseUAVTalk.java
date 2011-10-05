34b667fdda828d54f8735c9f4b558fda30f1c13f
ligi <ligi@ligi.de>
2011-10-05 13:06:22 +0200

    rm unused import

--- src/org/ligi/android/dubwise_uavtalk/DUBwiseUAVTalk.java
+++ src/org/ligi/android/dubwise_uavtalk/DUBwiseUAVTalk.java
@@ -40,7 +40,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.Window;
 
 /**
  * MainMenu/Entry/DashBoard Activity for DUBwise UAVTalk
