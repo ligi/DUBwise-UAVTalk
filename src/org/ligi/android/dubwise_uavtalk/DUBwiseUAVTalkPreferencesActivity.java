d731519f73811d0fe68812cec17741c175a2e8af
ligi <ligi@ligi.de>
2011-10-23 16:31:41 +0200

    make the preferences fit ux-wise

--- src/org/ligi/android/dubwise_uavtalk/DUBwiseUAVTalkPreferencesActivity.java
+++ src/org/ligi/android/dubwise_uavtalk/DUBwiseUAVTalkPreferencesActivity.java
@@ -4,13 +4,17 @@ import org.ligi.android.uavtalk.dubwise.R;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.View;
 
 public class DUBwiseUAVTalkPreferencesActivity extends PreferenceActivity {
 
 	  @Override
       protected void onCreate(Bundle savedInstanceState) {
 		  DUBwiseUAVTalkActivityCommons.before_content(this);    
+		  
 		  super.onCreate(savedInstanceState);
 		  addPreferencesFromResource(R.xml.preferences);
+		  this.setContentView(R.layout.list);
+		  getListView().setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 	  }
 }
